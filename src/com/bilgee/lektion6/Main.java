64b0109bb2a0780d8255ab95cf3e79ca43cc64a4
Bilgesti <144493145+Bilgesti@users.noreply.github.com>
2023-09-28 10:40:16 +0200

    Clean state

--- src/com/bilgee/lektion6/Main.java
+++ src/com/bilgee/lektion6/Main.java
@@ -1,7 +1,37 @@
 package com.bilgee.lektion6;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        //int[] numbers = {0, 100, 1000, 2000};
+
+        // Scanner in = new Scanner(System.in);
+        // System.out.println("Type something to crash the program");
+        // in.nextLine();
+
+       // for (int i = 0; i < numbers.length + 1 ; i++){
+       //     System.out.println(numbers[i]);
+       // }
+
+        System.out.println("Before crash");
+        try {
+
+            // try to execute code
+            System.out.println("Inside - TRY ");
+            System.out.println(2/0);
+
+        }catch (Exception error) {
+            // Catch error if it FAILS
+            System.out.println("Inside - CATCH ");
+            error.printStackTrace();
+        }
+
+
+        System.out.println("After crash");
+
+
 
     }
 }
