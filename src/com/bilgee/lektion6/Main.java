b67041b7d5f54a307aaa1defd43c3865e69859c8
Bilgesti <144493145+Bilgesti@users.noreply.github.com>
2023-09-28 11:01:25 +0200

    problemlosning

--- src/com/bilgee/lektion6/Main.java
+++ src/com/bilgee/lektion6/Main.java
@@ -1,3 +1,39 @@
 package com.bilgee.lektion6;
+public class Main {
+    /*TODO
+
+     */
+
+    /*TODO- E-Commerce APP
+        Order Product
+        Pay for product
+        Ship product
+        end
+
+    */
+
+    // #1 orderProduct()
+    // #2 payForProduct()
+    // #3 shipProduct()
+    // #4 sendConfirmationEmail()
+
+    // Initialize Project
+    // Setup Variables
+    // SetupGame()
+        // inputPlayerName()
+        // inputAmountOfDice()
+
+    // player
+        // int score;
+        // throwDice()
+
+    // scoreLogic()
+        // determineWinner()
+        // calculateScore()
+
+
+
+}
+
 
 
